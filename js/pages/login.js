a9129d462c96f78940ef53fd44ccff9e92a53ce3
Onogauko <156558403+Onogauko@users.noreply.github.com>
2026-06-26 04:43:23 +0700

    login.js

--- js/pages/login.js
+++ js/pages/login.js
@@ -1,3 +1,30 @@
+import { authService } from '../services/AuthService.js';
+
 export function initLoginPage() {
     console.log("Login page initialized");
+
+    const form = document.getElementById("login-form");
+
+    if (!form) {
+        console.error("Form login tidak ditemukan");
+        return;
+    }
+
+    form.addEventListener("submit", async (e) => {
+        e.preventDefault();
+
+        const userId = document.getElementById("user-id").value.trim();
+        const password = document.getElementById("password").value;
+
+        console.log("Login:", userId);
+
+        try {
+            const result = await authService.login(userId, password);
+
+            console.log(result);
+
+        } catch (err) {
+            console.error(err);
+        }
+    });
 }
